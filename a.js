06525872971bfb35d9930a5bc0ac9fd253fcafed
shoaibahmed2755 <168957132+shoaibahmed2755@users.noreply.github.com>
2025-03-27 23:35:14 +0530

    lenovo

--- a.js
+++ a.js
@@ -1,2 +1,13 @@
-var arr = new Array(10,20,40,"Shoaib"); //using a array constructor
-console.log(arr);
+var toCalsius = function(){
+    var celsius = (fahrenheit - 32) * 5/9;
+    return celsius;
+}
+var fahrenheit = 100
+fahrenheit=parseInt(fahrenheit);
+if(isNaN(fahrenheit)){
+    alert("Invalid input");
+}
+else{
+    var celsius = toCalsius(fahrenheit);
+    console.log(`${fahrenheit}°F is equal to ${celsius.toFixed(2)}°C`);
+}
