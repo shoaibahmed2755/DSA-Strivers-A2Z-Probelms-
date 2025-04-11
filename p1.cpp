9d4b0d7c4557349e0ef0d11e1c29f5396b468b7b
shoaibahmed2755 <168957132+shoaibahmed2755@users.noreply.github.com>
2025-04-11 12:00:09 +0530

    lenovo

--- p1.cpp
+++ p1.cpp
@@ -1,5 +1,14 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-    cout<<"Hello World";
+
+int main() {
+    //cout << "Hello World" << endl;
+    int a[5] = {10, 20, 30, 40, 50};
+
+    for (int i = 0; i < sizeof(a)/sizeof(a[0]); i++) {
+        // Printing the address of each element
+        cout << "Address of a[" << i << "] = " << (a + i) << endl;
+    }
+
+    return 0;
 }
